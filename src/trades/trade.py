3e523110e1f75f394a889a8ebee749467defe903
Frederik <fjanh9@gmail.com>
2020-05-13 17:25:00 +0200

    trade execution template

--- src/trades/trade.py
+++ src/trades/trade.py
@@ -1,12 +1,20 @@
 import cbpro
+import math
+import string
 
 from abc import ABC, abstractmethod
+from utilities.product_infos import ProductInfos
 
 class Trade(ABC):
-    def __init__(self, authClient: cbpro.AuthenticatedClient):
-        self.authClient = authClient
+    def __init__(self, auth_client: cbpro.AuthenticatedClient, order_type: str, order_side: str, product: str):
         super().__init__()
+        self.auth_client = auth_client
+        self.order_type = order_type  # "limit" (="maker") or "market" (="taker")
+        self.order_side = order_side  # "buy" or "sell"
+        self.product = product  # e.g. "BTC-EUR"
 
-    @abstractmethod
-    def execute(self):
-        pass
+    def _round_funds(self, funds: float, currency: str) -> float:
+        min_amount = float(ProductInfos.min_sizes[currency])
+        factor = 1.0 / min_amount
+        return math.floor(funds * factor) / round(factor)
+        
