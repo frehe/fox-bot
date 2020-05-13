c46ffbc4a0fc4a2baa6f8a4ca763c9fedd8528ca
Frederik <fjanh9@gmail.com>
2020-05-13 17:22:55 +0200

    specific signal generators

--- src/signal_generators/buy_signal_generators/relative_drop_signal.py
+++ src/signal_generators/buy_signal_generators/relative_drop_signal.py
@@ -2,15 +2,16 @@ import time
 from datetime import datetime, timezone
 
 from signal_generators.buy_signal_generators.buy_signal_generator import BuySignalGenerator
+from signals.buy_signal import BuySignal
 
 
 class RelativeDropSignal(BuySignalGenerator):
-    def __init__(self, publicClient, product, timespan, drop_percentage, max_price_percentage):
+    def __init__(self, public_client, product, timespan, drop_percentage, max_price_percentage):
         """[summary]
 
         Arguments:
             BuySignalGenerator {[type]} -- [description]
-            publicClient {[type]} -- [description]
+            public_client {[type]} -- [description]
             product {[type]} -- [description]
             timespan {[type]} -- [description]
             drop_percentage {[type]} -- [description]
@@ -18,32 +19,35 @@ class RelativeDropSignal(BuySignalGenerator):
         """        
         self.drop_percentage = drop_percentage
         self.max_price_percentage = max_price_percentage
-        super(RelativeDropSignal, self).__init__(publicClient, product, timespan)
+        self.granularity = 60  # {60, 300, 900, 3600, 21600, 86400}
+        super(RelativeDropSignal, self).__init__(public_client, product, timespan)
     
-    def getSignal(self):
+    def getSignal(self) -> BuySignal:
         while not self.signal:
-            timestamp = self.publicClient.get_time()
-            current_epoch = timestamp['epoch']
-            # current_iso = timestamp['iso']
+            self.timestamp = self.public_client.get_time()
+            current_epoch = self.timestamp['epoch']
             past_start_time_iso = datetime.fromtimestamp(current_epoch - self.timespan, tz=timezone.utc).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
             past_end_time_iso = datetime.fromtimestamp(current_epoch - self.timespan + self.granularity, tz=timezone.utc).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
-            # current_start_time_iso = datetime.fromtimestamp(current_epoch - 2*self.granularity, tz=timezone.utc).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
-            # current_end_time_iso = datetime.fromtimestamp(current_epoch, tz=timezone.utc).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
 
             self.past_rate = self._getRate(past_start_time_iso, past_end_time_iso)
 
-            day_stats = self.publicClient.get_product_24hr_stats(self.product)
+            day_stats = self.public_client.get_product_24hr_stats(self.product)
 
             self.current_rate = float(day_stats['last'])
             self.day_low = float(day_stats['low'])
             self.day_high = float(day_stats['high'])
 
-            self.signal = self._relativeDropSignal(self.past_rate, self.current_rate)
+            self.signal = self._relativeDropSignal()
             
             time.sleep(self.granularity)
-        return True
+        
+        buy_signal = BuySignal()
+        buy_signal.signal['activated'] = True
+        buy_signal.signal['past_rate'] = self.past_rate
+        buy_signal.signal['current_rate'] = self.current_rate
+        return buy_signal
     
-    def _relativeDropSignal(self, past_rate, current_rate) -> bool:
+    def _relativeDropSignal(self) -> bool:
         """Issue a buy signal if relative drop is at least drop percentage
 
         Arguments:
@@ -52,8 +56,27 @@ class RelativeDropSignal(BuySignalGenerator):
         """
         price_dropped = (1.0 - self.drop_percentage) * self.past_rate > self.current_rate
         price_is_low = self.current_rate < self.day_low + self.max_price_percentage * (self.day_high - self.day_low)
+
+        print('------'+ self.timestamp['iso'] + '\nPast rate: ' + str(self.past_rate) + '\nCurrent rate: ' + str(self.current_rate))
+
         if price_dropped and price_is_low:
-            # Buy signal
+            print('Prices have dropped')
             return True
+        else:
+            print('Prices have not dropped')
+        # return True
+        # TODO: Remove above statement
         return False
+
+    def _getRate(self, start, end):
+        # get price
+        print('getting rates')
+        rates = self.public_client.get_product_historic_rates(
+            product_id=self.product,
+            start=start,
+            end=end,
+            granularity=self.granularity)
+        window_low = rates[0][1]
+
+        return window_low
 
---
+++ src/signal_generators/sell_signal_generators/relative_rise_signal.py
@@ -0,0 +1,68 @@
+import time
+from datetime import datetime, timezone
+
+from signals.buy_signal import BuySignal
+from signals.sell_signal import SellSignal
+
+from signal_generators.sell_signal_generators.sell_signal_generator import SellSignalGenerator
+
+from utilities.secrets import Secrets
+from utilities.my_websocket_client import MyWebsocketClient
+
+
+class RelativeRiseSignal(SellSignalGenerator):
+    def __init__(self, public_client, product, rise_percentage):
+        """[summary]
+
+        Arguments:
+            BuySignalGenerator {[type]} -- [description]
+            public_client {[type]} -- [description]
+            product {[type]} -- [description]
+            timespan {[type]} -- [description]
+            drop_percentage {[type]} -- [description]
+            max_price_percentage {[type]} -- Buy only if current price is at most low + percentage * (high - low)
+        """        
+        super(RelativeRiseSignal, self).__init__(public_client, product)
+
+        self.rise_percentage = rise_percentage
+        self.granularity = 1
+
+        self.wsClient = MyWebsocketClient(
+            products=product,
+            auth=True,
+            api_key=Secrets.key,
+            api_secret=Secrets.b64secret,
+            api_passphrase=Secrets.passphrase,
+            channels=[{ "name": "ticker"}])
+
+    def getSignal(self, buy_signal: BuySignal) -> SellSignal:
+        self.buy_rate = buy_signal.signal['current_rate']  # TODO: replace by info from trade log
+
+        self.wsClient.start()
+        while not self.signal:
+            # Get current price from websocket
+            self.current_rate = self.wsClient.latest_msg
+            if self.current_rate != None:
+                self.signal = self._relativeRiseSignal()
+            
+            time.sleep(self.granularity)
+        
+        self.wsClient.close()
+        sell_signal = SellSignal()
+        sell_signal.signal['activated'] = True
+
+        return sell_signal
+
+    def _relativeRiseSignal(self) -> bool:
+        price_rose = self.current_rate > (1.0 + self.rise_percentage) * self.buy_rate
+
+        print('------\nCurrent rate: ' + str(self.current_rate) + '\nBought at rate: ' + str(self.buy_rate))
+
+        if price_rose:
+            print('Prices have risen back up')
+            return True
+        else:
+            print('Prices have not risen back up')
+        # return True
+        # TODO: Remove above statement
+        return False
