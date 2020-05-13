ff71964d4a4193f7b7d9f93343cc2366e5414a59
Frederik <fjanh9@gmail.com>
2020-05-13 17:22:20 +0200

    expand risk allocator template

--- src/risk_allocators/risk_allocator.py
+++ src/risk_allocators/risk_allocator.py
@@ -1,13 +1,24 @@
 import cbpro
 
-from trades.trade import Trade
+from abc import ABC
+
+from signals.buy_signal import BuySignal
+from signals.sell_signal import SellSignal
+from signals.signal import Signal
+
+from trades.buy_trade import BuyTrade
+from trades.sell_trade import SellTrade
 
 
-class RiskAllocator():
-    def __init__(self, authClient: cbpro.AuthenticatedClient, product: str):
+class RiskAllocator(ABC):
+    def __init__(self, auth_client: cbpro.AuthenticatedClient, product: str):
+        self.auth_client = auth_client
         self.product = product
         self.available_buy = None
         self.available_sell = None
     
-    def createTrade() -> Trade:
+    def createBuyTrade(self, buy_signal: BuySignal) -> BuyTrade:
+        pass
+
+    def createSellTrade(self, sell_signal: SellSignal) -> SellTrade:
         pass
