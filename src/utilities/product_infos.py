5aef5e279393c93d4f9537a2a92aa6cb40a34714
Frederik <fjanh9@gmail.com>
2020-05-13 17:25:08 +0200

    add

---
+++ src/utilities/product_infos.py
@@ -0,0 +1,18 @@
+from utilities.my_public_client import MyPublicClient
+
+
+class ProductInfos():
+    min_sizes = {}
+
+    @staticmethod
+    def refresh(public_client: MyPublicClient, product: str):
+        buy_currency = product[:3]
+        base_currency = product[-3:]
+
+        currencies = public_client.get_currencies()
+        currency_list = [elem['id'] for _, elem in enumerate(currencies)]
+
+        ProductInfos.min_sizes[buy_currency] = currencies[currency_list.index(buy_currency)]['min_size']
+        ProductInfos.min_sizes[base_currency] = currencies[currency_list.index(base_currency)]['min_size']
+
+
