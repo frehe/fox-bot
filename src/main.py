e3f756fdd033b93e4c8bf8da92e7ad365147f53e
Frederik <fjanh9@gmail.com>
2020-05-13 17:22:08 +0200

    abstract outer loop

--- src/main.py
+++ src/main.py
@@ -1,16 +1,27 @@
 import cbpro
 
+from strategies.relative_drop_strategy import RelativeDropStrategy
+
+from signal_generators.buy_signal_generators.relative_drop_signal import RelativeDropSignal
+from signal_generators.buy_signal_generators.buy_signal_generator import BuySignalGenerator
+
+from risk_allocators.simple_percentage_risk_allocator import SimplePercentageRiskAllocator
+
 from utilities.secrets import Secrets
 
+
+product = 'BTC-EUR'
+
 public_client = cbpro.PublicClient()
-# Sandbox auth client
 auth_client = cbpro.AuthenticatedClient(
-    Secrets.key,
-    Secrets.b64secret,
-    Secrets.passphrase,
+    Secrets.sandbox_key,
+    Secrets.sandbox_b64secret,
+    Secrets.sandbox_passphrase,
     api_url="https://api-public.sandbox.pro.coinbase.com")
 
-
-time = public_client.get_time()
-print(time['iso'])
-print(auth_client.get_time())
+while True:
+    # Spawn N strategies
+    # Wait until a strategy returns True, spawn a new one
+    strategy = RelativeDropStrategy(product, public_client, auth_client, 0.2)
+    strategy.execute()
+    print('Strategy done')
