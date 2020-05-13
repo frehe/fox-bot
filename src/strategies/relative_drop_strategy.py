bf9a369519900d87af4662dd1ffe652acbd01187
Frederik <fjanh9@gmail.com>
2020-05-13 17:24:37 +0200

    add complete init

--- src/strategies/relative_drop_strategy.py
+++ src/strategies/relative_drop_strategy.py
@@ -1,15 +1,30 @@
+import cbpro
+
 from strategies.strategy import Strategy
 
+from signal_generators.buy_signal_generators.relative_drop_signal import RelativeDropSignal
+from signal_generators.sell_signal_generators.relative_rise_signal import RelativeRiseSignal
+from risk_allocators.simple_percentage_risk_allocator import SimplePercentageRiskAllocator
+
 class RelativeDropStrategy(Strategy):
-    def __init__(self, signal_generator, risk_allocator):
+    def __init__(self, product: str, public_client: cbpro.PublicClient, auth_client: cbpro.AuthenticatedClient, p: float):
         """Initialize the relative drop strategy
 
         Arguments:
-            Strategy {Strategy} -- A general template for any trading strategy
-            signal_generator {SignalGenerator} -- A specific signal generator
-            risk_allocator {RiskAllocator} -- A specific risk allocator
-        """
-        super(RelativeDropStrategy, self).__init__(signal_generator, risk_allocator)
+            Strategy {[type]} -- [description]
+            product {str} -- [description]
+            public_client {cbpro.PublicClient} -- [description]
+            auth_client {cbpro.AuthenticatedClient} -- [description]
+            p {float} -- [description]
+        """        
+        buy_signal_generator = RelativeDropSignal(public_client, product, 24*60*60, p, 0.7)
+        sell_signal_generator = RelativeRiseSignal(public_client, product, 0.1)
+        risk_allocator = SimplePercentageRiskAllocator(auth_client, product, 0.1)
+        super(RelativeDropStrategy, self).__init__(
+            buy_signal_generator,
+            sell_signal_generator,
+            risk_allocator,
+            product,
+            public_client,
+            auth_client)
     
-    def run_strategy(self):
-        pass
