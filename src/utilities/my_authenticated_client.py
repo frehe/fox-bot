821e567e9a627b132e70bacbcb0aed4a19a116df
Frederik <fjanh9@gmail.com>
2020-05-14 15:56:47 +0200

    formatting

--- src/utilities/my_authenticated_client.py
+++ src/utilities/my_authenticated_client.py
@@ -3,4 +3,5 @@ from cbpro.authenticated_client import AuthenticatedClient
 
 class MyAuthenticatedClient(AuthenticatedClient):
     def __init__(self, key, b64secret, passphrase, api_url="https://api.pro.coinbase.com"):
-        super(MyAuthenticatedClient, self).__init__(key, b64secret, passphrase, api_url)
+        super(MyAuthenticatedClient, self).__init__(
+            key, b64secret, passphrase, api_url)
