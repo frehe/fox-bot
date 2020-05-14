ed6f5936a8e3664c229b331870b2f9540c8c48fd
Frederik <fjanh9@gmail.com>
2020-05-14 22:00:20 +0200

    backtesting

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ from strategies.relative_drop_strategy import RelativeDropStrategy
 
 from utilities.secrets import Secrets
 
+from backtesting.backtest import Backtest
+
 
 product = 'BTC-EUR'
 
@@ -14,6 +16,11 @@ auth_client = cbpro.AuthenticatedClient(
     Secrets.sandbox_b64secret,
     Secrets.sandbox_passphrase,
     api_url="https://api-public.sandbox.pro.coinbase.com")
+
+
+a = Backtest('EUR', 'BTC', '2020-02-20T14:00:00.000000', '2020-02-25T14:00:00.000000', 'GDAX')
+a.loadPriceData()
+
 
 # Spawn N strategies
 # Wait until a strategy returns True, spawn a new one
