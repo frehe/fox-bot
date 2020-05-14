79a0e0c0ea6ffaaff54b09a6f4fbf12f736e0b67
Frederik <fjanh9@gmail.com>
2020-05-14 19:08:20 +0200

    add print status

--- src/signal_generators/signal_generator.py
+++ src/signal_generators/signal_generator.py
@@ -4,11 +4,16 @@ from abc import ABC, abstractmethod
 
 
 class SignalGenerator(ABC):
-    def __init__(self, public_client: cbpro.PublicClient, product: str, timespan: int=0):
+    def __init__(
+            self, public_client: cbpro.PublicClient, product: str,
+            timespan: int = 0):
         self.public_client = public_client
         self.product = product
         self.timespan = timespan
         self.signal = False
 
     def getSignal(self):
+        pass
+
+    def _printStatus(self):
         pass
