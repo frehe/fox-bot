808028c1bba604a02ebfdd5158e1014138d5cce4
Frederik <fjanh9@gmail.com>
2020-05-15 13:54:45 +0200

    add custom cbpro client

---
+++ src/clients/auth_clients/cbpro_authenticated_client.py
@@ -0,0 +1,112 @@
+from cbpro.authenticated_client import AuthenticatedClient
+
+from clients.my_authenticated_client import MyAuthenticatedClient
+
+
+class CBProAuthenticatedClient(MyAuthenticatedClient):
+    def __init__(
+            self, key: str, b64secret: str, passphrase: str,
+            api_url="https://api.pro.coinbase.com"):
+
+        super(CBProAuthenticatedClient, self).__init__(
+            key, b64secret, passphrase, api_url)
+
+        self.auth_client = AuthenticatedClient(
+            key=key,
+            b64secret=b64secret,
+            passphrase=passphrase,
+            api_url=api_url
+        )
+
+    def get_time(self) -> dict:
+        """[summary]
+
+        Returns:
+            dict -- Dict {
+                'iso': 'YYYY-MM-DDThh:mm:ss.fffZ',
+                'epoch': UNIX epoch
+                }
+        """
+        return self.auth_client.get_time()
+
+    def get_accounts(self) -> list:
+        """Get a summary of all accounts with different currencies.
+
+        Returns:
+            list -- List of dicts containing keys
+                            ['id', 'currency' 'balance', 'available']
+        """
+        return self.auth_client.get_accounts()
+
+    def buy_taker(self, product: str, funds: str) -> dict:
+        """Issue a buy order on the taker (market) side.
+
+        Arguments:
+            product {str} -- e.g. 'BTC-EUR'
+            funds {float} -- The amount of funds in base currency to use
+
+        Returns:
+            dict -- with keys ['id', 'product_id', 'side', 'funds',
+                'specified_funds', 'executed_value', 'filled_size', 'type',
+                'created_at', 'done_at', 'fill_fees', 'status', 'settled']
+        """
+        return self.auth_client.place_market_order(
+            product_id=product,
+            side='buy',
+            funds=funds
+        )
+
+    def buy_maker(self, product: str, price: str, size: str) -> dict:
+        """Issue a buy order on the maker (limit) side.
+
+        Arguments:
+            product {str} -- [description]
+            price {str} -- [description]
+            size {str} -- [description]
+
+        Returns:
+            dict -- [description]
+        """
+        pass
+
+    def sell_taker(self, product: str, size: str) -> dict:
+        """Issue a sell order on the taker (market) side.
+
+        Arguments:
+            product {str} -- e.g. 'BTC-EUR'
+            funds {float} -- The amount of funds in base currency to use
+
+        Returns:
+            dict -- [description]
+        """
+        return self.auth_client.place_market_order(
+            product_id=product,
+            side='sell',
+            size=size
+        )
+
+    def sell_maker(self, product: str, price: str, size: str) -> dict:
+        """Issue a sell order on the maker (limit) side.
+
+        Arguments:
+            product {str} -- [description]
+            price {str} -- [description]
+            size {str} -- [description]
+
+        Returns:
+            dict -- [description]
+        """
+        pass
+
+    def get_order(self, id: str) -> dict:
+        """Get updated information on a placed order
+
+        Arguments:
+            id {str} -- The id by with to identify the order.
+
+        Returns:
+            dict -- Dictionary with keys ['id', 'product_id', 'side', 'funds',
+                'specified_funds', 'executed_value', 'filled_size', 'type',
+                'created_at', 'done_at', 'fill_fees', 'status', 'settled']
+        """
+        return self.auth_client.get_order(id)
---
+++ src/clients/public_clients/cbpro_public_client.py
@@ -0,0 +1,67 @@
+from cbpro.public_client import PublicClient
+
+from clients.my_public_client import MyPublicClient
+
+
+class CBProPublicClient(MyPublicClient):
+    def __init__(self, api_url='https://api.pro.coinbase.com', timeout=30):
+        super(CBProPublicClient, self).__init__(api_url, timeout)
+
+        self.public_client = PublicClient(api_url, timeout)
+
+    def get_time(self) -> dict:
+        """Get current time from server.
+
+        Returns:
+            dict -- Dict {
+                'iso': 'YYYY-MM-DDThh:mm:ss.fffZ',
+                'epoch': UNIX epoch
+                }
+        """
+        return self.public_client.get_time()
+
+    def get_currencies(self) -> list:
+        """List all known currencies.
+
+        Returns:
+            list -- List of dicts [
+                {'id': 'BTC', 'name': 'Bitcoin', 'min_size': '0.00000001'},
+                {'id': 'EUR', 'name': 'Euro', 'min_size': '0.01'},
+                ...
+                ]
+        """
+        return self.public_client.get_currencies()
+
+    def get_product_24hr_stats(self, product: str) -> dict:
+        """Get 24hr stats of a given product.
+
+        Arguments:
+            product {str} -- e.g. 'BTC-EUR'
+
+        Returns:
+            dict -- Dict containing keys 'open', 'high', 'low', 'volume', 'last'
+        """
+        return self.public_client.get_product_24hr_stats(product)
+
+    def get_product_historic_rates(
+            self, product: str, start: str,
+            end: str, granularity: int) -> list:
+        """Obtain historic rate information between two timepoints.
+
+        Arguments:
+            product {str} -- e.g. 'BTC-EUR'
+            start {str} -- ISO 8601-formatted: 'YYYY-MM-DDThh:mm:ss.fffZ'
+            end {str} -- ISO 8601-formatted: 'YYYY-MM-DDThh:mm:ss.fffZ'
+            granularity {int} -- one of {60, 300, 900, 3600, 21600, 86400}
+
+        Returns:
+            list -- List of buckets. Each bucket contains data
+                [time, low, high, open, close, volume] for the
+                duration of granularity
+        """
+        return self.public_client.get_product_historic_rates(
+            product_id=product,
+            start=start,
+            end=end,
+            granularity=granularity
+        )
