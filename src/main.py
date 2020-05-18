a420ba8211770716022fca809f30cd97c38f2738
Frederik <fjanh9@gmail.com>
2020-05-18 23:46:06 +0200

    init backtest client

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import time
 
 from clients.auth_clients.cbpro_authenticated_client import CBProAuthenticatedClient
 from clients.public_clients.cbpro_public_client import CBProPublicClient
+from clients.auth_clients.backtesting_authenticated_client import BacktestingAuthenticatedClient
+from clients.public_clients.backtesting_public_client import BacktestingPublicClient
 
 from strategies.relative_drop_strategy import RelativeDropStrategy
 
@@ -12,18 +14,19 @@ from backtesting.backtesting_engine import BacktestingEngine
 
 product = 'BTC-EUR'
 
-public_client = CBProPublicClient()
 auth_client = CBProAuthenticatedClient(
     key=Secrets.sandbox_key,
     b64secret=Secrets.sandbox_b64secret,
     passphrase=Secrets.sandbox_passphrase,
     api_url="https://api-public.sandbox.pro.coinbase.com")
+public_client = CBProPublicClient()
 
+auth_client = BacktestingAuthenticatedClient(
+    '2020-02-20T14:00:00.000000', '2020-02-25T14:00:00.000000',
+    60, {'EUR': 60.0}, 0.005, 0.005)
 
-a = BacktestingEngine(
-    '2020-02-20T14:00:00.000000', '2020-02-25T14:00:00.000000', 60,
-    {}, 0.005, 0.005)
-a.loadPriceData()
+auth_client.Engine.loadPriceData('BTC-EUR')
+
 
 
 # Spawn N strategies
