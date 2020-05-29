5995a7c027d269cca99883ed68a6f7d0c0e8e0e5
Frederik <fjanh9@gmail.com>
2020-05-29 15:42:18 +0200

    init testing of clients

---
+++ src/utilities/tests/test_auth_clients.py
@@ -0,0 +1,9 @@
+import unittest
+
+from clients.auth_clients.my_authenticated_client import MyAuthenticatedClient
+from clients.auth_clients.cbpro_authenticated_client import CBProAuthenticatedClient
+from clients.auth_clients.backtesting_authenticated_client import BacktestingAuthenticatedClient
+
+
+class TestClient(unittest.TestCase):
+    pass
